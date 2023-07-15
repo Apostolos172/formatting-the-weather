97dda21e0b6fc23fa5ebfd33bb8181c59662aba2
Apostolos172 <apostolos.vad@gmail.com>
2023-07-15 18:09:20 +0300

    test

---
+++ date.js
@@ -0,0 +1,5 @@
+let date = moment();
+let currentDate = date.format("D/M");
+// day1today.textContent = currentDate;
+// day2tomorrow.textContent = currentDate.add(1, "day");
+console.log(currentDate); // "17/06/2022"
